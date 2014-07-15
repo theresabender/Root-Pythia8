f5f263a414ccfec220cceadbf3cc68121302388e
theresabender <test@130-199-21-171.dhcp.bnl.gov>
2014-07-15 11:44:57 -0400

    cleanup

--- pythia8.C
+++ pythia8.C
@@ -40,8 +40,6 @@ void pythia8(Int_t nev  = 100, Int_t ndeb = 1){
    gSystem->Load("libEG");
    gSystem->Load("libEGPythia8");
 // Histograms
-   //TH1F* etaH = new TH1F("etaH", "Pseudorapidity", 120, -12., 12.);
-   //TH1F* ptH  = new TH1F("ptH",  "pt",              50,   0., 10.);
     TH1F* yH = new TH1F("etaH", "Higgs pseudorapidity", 100, -5., 5.);
     TH1F* ptH  = new TH1F("ptH",  "Higgs transverse momentum", 100, 0., 200.);
     
@@ -53,9 +51,6 @@ void pythia8(Int_t nev  = 100, Int_t ndeb = 1){
    TPythia8* pythia8 = new TPythia8();
     
 // Configure    
-    // pythia8->ReadString("SoftQCD:minBias = on");
-   //   pythia8->ReadString("SoftQCD:singleDiffractive = on");
-   //   pythia8->ReadString("SoftQCD:doubleDiffractive = on");
     pythia8->ReadString("Beams:eCM = 14000.");
     pythia8->ReadString("HiggsSM:gg2H = on");
     
@@ -63,28 +58,14 @@ void pythia8(Int_t nev  = 100, Int_t ndeb = 1){
     
    pythia8->Initialize(2212 /* p */, 2212 /* p */, 14000. /* TeV */);
 
-    //Hist pTH("Higgs transverse momentum", 100, 0., 200.);
-    //Hist etaH("Higgs pseudorapidity", 100, -5., 5.);
 
 // Event loop
-    //cout << "nev=" << nev << endl;
-    /// here: declare the array
-//    Int_t pdg_array[20000];
-//    Int_t pdg_array_idx=-1;
 
-// Write out histograms
-//    cout << ptH << yH;
-
-//    vector<Int_t> pdg_vector;
-
-cout << nev << endl;
     for (Int_t iev = 0; iev < nev; iev++) {
       pythia8->GenerateEvent();
       if (iev < ndeb) pythia8->EventListing();
       pythia8->ImportParticles(particles,"All");
       Int_t np = particles->GetEntriesFast();
-          
-//    int iH = 0;
         
 // Particle loop
       for (Int_t ip = 0; ip < np; ip++) {
@@ -100,14 +81,6 @@ cout << nev << endl;
               Float_t charge = TDatabasePDG::Instance()->GetParticle(pdg)->Charge();
           }
 
-//         if (pdg == 211){
-//              yH->Fill(eta);
-//              if (pt > 0.) ptH->Fill(pt, 1./(2. * pt));
-//         }
-         
-//         for (int iEvent = 0; iEvent < 10; ++iEvent){
-//                pythia8->next();
-//         } // for iEvent < 10
           
           if (pdg == 25){
               if (pt > 0.){
@@ -116,49 +89,11 @@ cout << nev << endl;
                   if (iev % 100 == 1)
                       cout << "pdg=" << pdg << " Transverse momentum: "<< pt << " Rapidity: " << eta << endl ;
               }
-              /// print values of final Higgs
-          }
+              }
 
-          
-          
-//         for (int i = 0; i < pythia8->event.size(); ++i){
-//            cout << "i = " << i
-//                << ", id = " << pythia8->event[i].id() << endl;
-//            pythia8->stat();
-//         } // for i <pythia8->event.size
-//        
-//          cout << "i=" << ip << " id=" << pdg << endl;
-          
-//          /// print values of final Higgs
-//         cout << "Transverse momentum: "<< pythia8->event[i].pT() << "Rapidity: " << pythia.event[i].y() ;
-          
-//          if (ist>0){
-//              cout << "pdg="<< pdg << " status:"<< ist << endl;
-//          
-//              if (pdg != 211 && pdg != -211){
-//                  cout << "............ pdg=" << pdg << endl;
-//                  //// here: fill the array
-//                  pdg_array_idx += 1;
-//                  pdg_array[pdg_array_idx]= pdg;
-//                  pdg_vector.push_back(pdg);
-//              }
-//              
-//          }
           
       } // for .. particle loop
        
-//       /// here: print the array
-//       //// do for-loop to print every element of the array
-//        cout << "We stored "<< pdg_array_idx << " values of pdg." << endl;
-//        for(Int_t li=0; li < pdg_array_idx; li++){
-//            cout << "["<< li <<"]: pdg=" << pdg_array[li] << endl;
-//            cout << "["<< li <<"]: pdg=" << pdg_vector[li] << endl;
-//        }
-//        
-//        for (vector<Int_t>::iterator iter = pdg_vector.begin() ; iter != pdg_vector.end(); ++iter){
-//        
-//            cout << "pdg=" << pdg_vector[*iter] << endl;
-//        }
         
    } // for .. event loop
 
